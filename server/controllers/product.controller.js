ffcca6b93ba5a29439764e57f04fd3168680b46b
VictorCasco-PY <cascoalmada@gmail.com>
2023-02-13 18:43:55 -0300

    Se agrego pagina de detalles

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,5 +1,6 @@
 const { Product } = require("../models/product.model");
 
+//Crear un nuevo producto
 module.exports.createProduct = async (request, response) => {
   try {
     const { tittle, price, description } = request.body;
@@ -9,6 +10,26 @@ module.exports.createProduct = async (request, response) => {
       description,
     });
     response.json(product);
+  } catch (error) {
+    response.json(error);
+  }
+};
+
+//Listar todos los productos
+module.exports.findAllProducts = async (request, response) => {
+  try {
+    const productList = await Product.find({});
+    response.json(productList);
+  } catch (error) {
+    response.json(error);
+  }
+};
+
+//Mostrar un solo producto por ID
+module.exports.getProduct = async (request, response) => {
+  try {
+    const productId = await Product.findOne({ _id: request.params.id });
+    response.json(productId);
   } catch (error) {
     response.json(error);
   }
